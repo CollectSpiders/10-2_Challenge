cc68e990e20997f967c9446333a7d07b30b91c54
CollectSpiders <98413464+CollectSpiders@users.noreply.github.com>
2023-04-02 20:49:57 -0700

    completed the triangle and square options in generateLogo

--- Assets/generateLogo.js
+++ Assets/generateLogo.js
@@ -15,15 +15,29 @@ function generateLogo(data) {
 
         // if shape is square return ___
         case 'Square':
-            return ``
+            return `
+                <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+                <rect width="300" height="200" fill="${data.color}" />
+
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${data.characters}</text>
+
+                </svg>`
             break;
 
         // if shape is triangle return ___
         case 'Triangle':
-            return ``
+            return `
+                <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+                <polygon points="150,0 0,200 300,200" fill="${data.color}" stroke="black"/>
+
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${data.characters}</text>
+
+                </svg>`
             break;
     }
 
-};
+}
 
 module.exports = generateLogo;
